fa89227f531a8444614a2d3021940e851e7dfe1b
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-13 21:11:57 +0200

    Refactoring.

--- src/main/java/com/trl/microservicee/app/AppController.java
+++ src/main/java/com/trl/microservicee/app/AppController.java
@@ -1,12 +1,13 @@
-package com.trl.microservicee.hello;
+package com.trl.microservicee.app;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/hello")
-public class HelloController {
+@RequestMapping(path = "/app")
+public class AppController {
 
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -14,33 +15,40 @@ public class HelloController {
     public ResponseEntity<String> post() {
         ResponseEntity<String> response = null;
 
+        response = ResponseEntity.status(HttpStatus.CREATED).body("(microservice-e)(post)");
+
         return response;
     }
 
     @GetMapping(
-            path = "/{helloId}",
+            path = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> get() {
+    public ResponseEntity<String> get(@PathVariable Long id) {
         ResponseEntity<String> response = null;
+
+        response = ResponseEntity.status(HttpStatus.OK).body("(microservice-e)(get)(id=" + id + ")");
 
         return response;
     }
 
-    @PatchMapping(path = "/{helloId}",
+    @PatchMapping(path = "/{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> patch() {
+    public ResponseEntity<String> patch(@PathVariable Long id) {
         ResponseEntity<String> response = null;
+
+        response = ResponseEntity.status(HttpStatus.OK).body("(microservice-e)(patch)(id=" + id + ")");
 
         return response;
     }
 
     @DeleteMapping(
-            path = "/{helloId}",
+            path = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> delete() {
+    public ResponseEntity<String> delete(@PathVariable Long id) {
         ResponseEntity<String> response = null;
 
+        response = ResponseEntity.status(HttpStatus.OK).body("(microservice-e)(patch)(id=" + id + ")");
 
         return response;
     }
